0f8a8765608b6251580e4cdb57b2d1fa3374bf81
chest <chester_csy@outlook.com>
2021-01-29 10:44:25 +0800

    1

---
+++ src/main/java/com/chester/svc/ChesterSvcPackApplication.java
@@ -0,0 +1,13 @@
+package com.chester.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesterSvcPackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChesterSvcPackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chester/svc/ChesterSvcPackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chester.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesterSvcPackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
